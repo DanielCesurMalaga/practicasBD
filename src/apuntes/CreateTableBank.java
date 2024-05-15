0664b77ce839dcfaae147a43e7b067a659ca8c7d
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-05-15 12:28:18 +0200

    usando loader deprecated

--- src/apuntes/CreateTableBank.java
+++ src/apuntes/CreateTableBank.java
@@ -1,6 +1,8 @@
 package apuntes;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -15,6 +17,14 @@ public class CreateTableBank {
          "password VARCHAR(20) NOT NULL, " +
          "balance Integer NOT NULL, " +
          "PRIMARY KEY(client))";
+
+   String createInsertInto = "INSERT INTO BANCO " +
+         "(client,password,balance) VALUES " +
+         "('Jose Antonio','c',10)";
+
+   String createSelect = "select * from banco";
+   // insertar un elemento.
+   // seleccionar un elemento.
    Connection connection;
    Statement statement;
 
@@ -48,6 +58,33 @@ public class CreateTableBank {
          statement.executeUpdate(createTableBank);
       } catch (SQLException e) {
          System.out.println("al crear la tabla");
+         System.out.println(e.getMessage());
+      }
+   }
+
+   public void useStatementInsert(){
+      try {
+         statement.executeUpdate(createInsertInto);
+      } catch (SQLException e) {
+         System.out.println("al crear la tabla");
+         System.out.println(e.getMessage());
+      }
+   }
+
+   public void useSelect(){
+      try {
+         ResultSet vista = statement.executeQuery(createSelect);
+         ResultSetMetaData datosVista = vista.getMetaData();
+         System.out.println("El nombre de la tabla es: "+ datosVista.getTableName(1));
+         System.out.println("Número de filas:  "+ vista.getFetchSize());
+         while (vista.next()){
+            for (int i = 1; i <= datosVista.getColumnCount(); i++) {
+               System.out.print("["+datosVista.getColumnName(i)+" : " + vista.getString(i)+"] ");
+            }
+            System.out.println();
+         }
+         System.out.println("El puntero al final apunta a: "+ vista.getRow());
+      } catch (SQLException e) {
          System.out.println(e.getMessage());
       }
    }
