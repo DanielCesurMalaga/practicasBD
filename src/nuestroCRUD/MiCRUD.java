1e906f0587865729c6fece0f875b975265c5c295
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-05-15 13:18:44 +0200

    Adrian se tiene q ir otra vez

--- src/nuestroCRUD/MiCRUD.java
+++ src/nuestroCRUD/MiCRUD.java
@@ -60,7 +60,37 @@ public class MiCRUD {
 
    // create
    public boolean createTable(String name, MyColumn[] columns, MyConstraint[] constraints){
+      String myQuery = "CREATE TABLE ".concat(name)+" (";
 
+      for (int i = 0; i < columns.length; i++) {
+         myQuery = myQuery + columns[i].getColName() + " " + columns[i].getColType() + " ";
+         if (columns[i].isNulleable()) {
+            myQuery = myQuery + ",";
+         } else {
+            myQuery = myQuery + " NOT NULL"+",";
+         }
+
+         // there is always at least one constraint for the last of type primary key,
+         
+      }
+
+      for (int j = 0; j < constraints.length-1; j++) {
+         myQuery = myQuery + " CONSTRAINT " + constraints[j].getParams()[0];
+         if (constraints[j].getParams().length == 2) { // primary key
+            myQuery = myQuery + " PRIMARY KEY (";
+            myQuery = myQuery + constraints[j].getParams()[1] + "),";
+         } else { // foreign key
+            myQuery = myQuery + " FOREIGN KEY (";
+            myQuery = myQuery + constraints[j].getParams()[1] + ")";
+            myQuery = myQuery + " REFERENCES ";
+            myQuery = myQuery + constraints[j].getParams()[2] + "(";
+            myQuery = myQuery + constraints[j].getParams()[3] + "),";
+
+         }
+         
+      }
+
+      // copy and paste for the last element of constraints
    }
    // read
    // update
