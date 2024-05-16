c59a42c300e99b4cfe4ba610f433709bb8ca3bff
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-05-16 12:23:27 +0200

    testeada la conexion a BD

--- src/App.java
+++ src/App.java
@@ -1,15 +1,29 @@
 import java.sql.SQLException;
 
-import apuntes.CreateTableBank;
+import nuestroCRUD.MiCRUD;
 
 public class App {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        CreateTableBank miTabla = new CreateTableBank();
-        miTabla.initConnection();
-        miTabla.createStatement();
-        miTabla.useSelect();
-
-        miTabla.closeConnection();
-
+    public static void main(String[] args) {
+        
+        MiCRUD miAcceso = new MiCRUD("banco");
+        System.out.println("Creado objeto y url actualizada : " + miAcceso.getUrl());
+        boolean continuar = miAcceso.initDriver();
+        if (continuar){
+            System.out.println("Driver iniciado correctamente.");
+        } else {
+            System.out.println("Error al iniciar el Driver.");
+        }
+        continuar = miAcceso.initConnection();
+        if (continuar){
+            System.out.println("Se ha establecido conexión con la BD.");
+        } else {
+            System.out.println("Error en la conexión con la BD");
+        }
+        continuar = miAcceso.createStatement();
+        if (continuar){
+            System.out.println("Se ha creado el Statement correctamente.");
+        } else {
+            System.out.println("NO Se ha creado el Statement correctamente.");
+        }
     }
 }
--- src/nuestroCRUD/MiCRUD.java
+++ src/nuestroCRUD/MiCRUD.java
@@ -7,74 +7,80 @@ import java.sql.Statement;
 
 public class MiCRUD {
    // atributtes
-   private String driver = "com.mysql.jdbc.Driver";
+   private final String DRIVER = "com.mysql.cj.jdbc.Driver";
    private String url = "jdbc:mysql://localhost:3306/";
    private String user = "root";
    private String password = "";
    private Connection connection;
    private Statement statement;
-   
+
+   public String getUrl() {
+      return this.url;
+   }
+
    // constructor
-   public MiCRUD(String baseDeDatos){
+   public MiCRUD(String baseDeDatos) {
       this.url = this.url.concat(baseDeDatos);
    };
 
-   public boolean initDriver(){
+   public boolean initDriver() {
       try {
-         Class.forName(driver);
+         Class.forName(DRIVER);
          return true;
       } catch (ClassNotFoundException e) {
          return false;
       }
    }
 
-   public boolean initConnection(){
+   public boolean initConnection() {
       this.connection = null;
       try {
          this.connection = DriverManager.getConnection(url, user, password);
          return true;
       } catch (SQLException e) {
          return false;
-      } 
+      }
    }
 
-   public boolean closeConnection(){
+   public boolean closeConnection() {
       try {
-            this.connection.close();
-            return true;
+         this.connection.close();
+         return true;
       } catch (SQLException e) {
          return false;
-      } catch (NullPointerException e){
+      } catch (NullPointerException e) {
          return false;
       }
    }
 
-   public boolean createStatement(){
+   public boolean createStatement() {
       try {
          this.statement = connection.createStatement();
          return true;
       } catch (SQLException e) {
          return false;
+      } catch (NullPointerException e) {
+         return false;
       }
    }
 
    // create
-   public boolean createTable(String name, MyColumn[] columns, MyConstraint[] constraints){
-      String myQuery = "CREATE TABLE ".concat(name)+" (";
+   public boolean createTable(String name, MyColumn[] columns, MyConstraint[] constraints) {
+      String myQuery = "CREATE TABLE " + name + " (";
 
       for (int i = 0; i < columns.length; i++) {
          myQuery = myQuery + columns[i].getColName() + " " + columns[i].getColType() + " ";
          if (columns[i].isNulleable()) {
             myQuery = myQuery + ",";
          } else {
-            myQuery = myQuery + " NOT NULL"+",";
+            myQuery = myQuery + " NOT NULL" + ",";
          }
 
          // there is always at least one constraint for the last of type primary key,
-         
+
       }
 
-      for (int j = 0; j < constraints.length-1; j++) {
+      for (int j = 0; j < constraints.length - 1; j++) {
          myQuery = myQuery + " CONSTRAINT " + constraints[j].getParams()[0];
          if (constraints[j].getParams().length == 2) { // primary key
             myQuery = myQuery + " PRIMARY KEY (";
@@ -87,9 +93,9 @@ public class MiCRUD {
             myQuery = myQuery + constraints[j].getParams()[3] + "),";
 
          }
-         
-      }
 
+      }
+      return true;
       // copy and paste for the last element of constraints
    }
    // read
