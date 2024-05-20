e82729562d6c1d5fc8966df56b60d2ccf9856248
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-05-20 13:05:03 +0200

    testeado el método createTable completo y ok

--- src/App.java
+++ src/App.java
@@ -8,24 +8,23 @@ public class App {
     public static void main(String[] args) {
         MiCRUD prueba = new MiCRUD("bank");
 
-        // 0- null o algo null tb.
-        // 0 constrains y 0 columnas
-        // 1- 0 columnas
-        //  1.1 con una constrain
-        //  1.2 con más de una constrain
-
-        // 2- 0 constrains
-        //  2.1 con una columna
-        //  2.2 con más de una columna.
-
         MyColumn[] columnas = new MyColumn[2];
         columnas[0] = new MyColumn();
         columnas[0].setColName("nombre");
         columnas[0].setColType("varchar(32)");
-        MyConstraint[] restricciones= new MyConstraint[5];
-        // MyConstraint[] restricciones = new MyConstraint[];
-        System.out.print("Salida: ");
-        System.out.println(prueba.createTable("mi tabla", columnas, restricciones));
+        columnas[1] = new MyColumn();
+        columnas[1].setColType("varchar(32)");
+        columnas[1].setNulleable(true);
+
+        MyConstraint[] restricciones= new MyConstraint[3];
+        restricciones[0] = new MyConstraint(true);
+        restricciones[0].setParams(new String[] {"pk_nombre","nombre"});
+        restricciones[1] = new MyConstraint(false);
+        restricciones[1].setParams(new String[] {"fk_nombre1","apellido1","habitacion1","codigo1"});
+        restricciones[2] = new MyConstraint(false);
+        restricciones[2].setParams(new String[] {"fk_nombre2","apellido2","habitacion2","codigo2"});
         
+        System.out.print("Salida: ");
+        System.out.println(prueba.createTable("mi tabla", columnas, restricciones));      
     }
 }
--- src/nuestroCRUD/MiCRUD.java
+++ src/nuestroCRUD/MiCRUD.java
@@ -80,15 +80,15 @@ public class MiCRUD {
             if (constActual==null) return null;
          }
       }
-      
+
       String myQuery = "CREATE TABLE " + name + " (";
 
       for (int i = 0; i < columns.length; i++) {
-         myQuery = myQuery + columns[i].getColName() + " " + columns[i].getColType() + " ";
+         myQuery = myQuery + columns[i].getColName() + " " + columns[i].getColType();
          if (columns[i].isNulleable()) {
-            myQuery = myQuery + ",";
+            myQuery = myQuery + ", ";
          } else {
-            myQuery = myQuery + " NOT NULL" + ",";
+            myQuery = myQuery + " NOT NULL" + ", ";
          }
 
          // there is always at least one constraint for the last of type primary key,
@@ -96,7 +96,7 @@ public class MiCRUD {
       }
 
       for (int j = 0; j < constraints.length - 1; j++) {
-         myQuery = myQuery + " CONSTRAINT " + constraints[j].getParams()[0];
+         myQuery = myQuery + "CONSTRAINT " + constraints[j].getParams()[0];
          if (constraints[j].getParams().length == 2) { // primary key
             myQuery = myQuery + " PRIMARY KEY (";
             myQuery = myQuery + constraints[j].getParams()[1] + ")";
@@ -107,9 +107,9 @@ public class MiCRUD {
             myQuery = myQuery + constraints[j].getParams()[2] + "(";
             myQuery = myQuery + constraints[j].getParams()[3] + ")";
          }
-         myQuery = myQuery + ",";
+         myQuery = myQuery + ", ";
       }
-      myQuery = myQuery + " CONSTRAINT " + constraints[constraints.length-1].getParams()[0];
+      myQuery = myQuery + "CONSTRAINT " + constraints[constraints.length-1].getParams()[0];
       if (constraints[constraints.length-1].getParams().length == 2) { // primary key
          myQuery = myQuery + " PRIMARY KEY (";
          myQuery = myQuery + constraints[constraints.length-1].getParams()[1] + ")";
